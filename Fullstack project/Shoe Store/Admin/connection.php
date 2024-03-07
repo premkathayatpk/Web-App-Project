60e4abb5fbac9ebfd7b8907230439b75bca255ff
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-03-07 17:20:28 +0545

    add login page

--- Fullstack project/Shoe Store/Admin/connection.php
+++ Fullstack project/Shoe Store/Admin/connection.php
@@ -1,15 +1,15 @@
-
 <?php
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "shoe_store";
+$dbname="shoe_store";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
+$conn = new mysqli($servername, $username, $password,$dbname);
 
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "";
 ?>
