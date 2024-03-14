c8c8a3e21f12d7eccaa94172e03ebaa75727614b
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-03-14 11:39:05 +0545

    add customer table

--- Fullstack project/Shoe Store/Admin/customer.php
+++ Fullstack project/Shoe Store/Admin/customer.php
@@ -1,26 +1,42 @@
 <?php
 require 'header.php'
 ?>
-
-<h1>Customers</h1>
+<?php include 'connection.php'?>
+<div class="customer">
+<center><h1>Customers</h1></center>
 
 <div class="customer-search">
     <input type="text" placeholder="search product here">
 </div>
 
 <div class="customer-table">
-    <table>
+    <table >
         <thead>
             <tr>
                 <th>Name</th>
                 <th>Address</th>
-                <th>Province</th>
+                <th>Country</th>
                 <th>Zipcode</th>
                 <th>Mobile</th>
-                <th>Telephone</th>
                 <th>Email</th>
             </tr>
         </thead>
-
+        <?php
+					$query = mysqli_query($conn, "SELECT * FROM `customer`") or die(mysqli_error());
+					while($fetch = mysqli_fetch_array($query))
+						{
+				?>
+                <tr>
+                <td><?php echo $fetch['firstname'];?>&nbsp;<?php echo  $fetch['lastname'];?></td>
+					<td><?php echo $fetch['address']?></td>
+					<td><?php echo $fetch['country']?></td>
+					<td><?php echo $fetch['zipcode']?></td>
+					<td><?php echo $fetch['mobile']?></td>
+					<td><?php echo $fetch['email']?></td>
+				</tr>		
+				<?php
+					}
+				?>
     </table>
+</div>
 </div>
