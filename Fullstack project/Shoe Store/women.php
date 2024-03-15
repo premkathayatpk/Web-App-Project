f4dda0d141be1824b478980ca96b72d1df9b0d49
Prem Kathayat <132916774+premkathayatpk@users.noreply.github.com>
2024-03-15 12:56:42 +0545

    women

--- Fullstack project/Shoe Store/women.php
+++ Fullstack project/Shoe Store/women.php
@@ -1,4 +1,5 @@
 <?php include 'header.php'?>
+<?php include 'Admin/connection.php'?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,7 +13,44 @@
 </head>
 
 <body>
+<!-- Main section -->
+<main>
+        <div class="items-container">
+        <?php
+ $select_brand = "select *from `product`";
+ $result_brand = mysqli_query($conn,$select_brand);
 
+
+while($row_data = mysqli_fetch_assoc($result_brand)){
+  $product_category =$row_data['category'];
+
+  $product_brand = $row_data['brand'];
+  $product_size = $row_data['product_size'];
+  $product_price =$row_data['product_price'];
+  $product_image =$row_data['product_image'];
+  $product_title =  $row_data['product_name'];
+
+ 
+  if($product_category== 'Women'){
+
+  echo " <div class='item-container'>
+  <img class='item-image' src='photo/$product_image' alt='item image'>
+  <div class='company-name'>$product_brand</div>
+  <div class='item-name'>$product_title</div>
+
+  <div class='size'>
+   Size: $product_size
+</div>
+  <div class='price'>
+    Rs. $product_price
+  </div>
+ 
+  <button class='btn-add-cart' onclick=''>Add to Cart</button>
+</div>";
+
+}
+ }
+?>
 </body>
 
 </html>
